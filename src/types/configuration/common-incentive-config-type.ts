816c48f398daacbd5ae84ccf31152fdcfdf2ce9e
gentlementlegen <fernand.veyrier@epitech.eu>
2024-04-25 12:41:36 +0900

    chore: exporting base incentive configuration

--- src/types/configuration/common-incentive-config-type.ts
+++ src/types/configuration/common-incentive-config-type.ts
@@ -1,9 +1,7 @@
 import { Type, Static } from "@sinclair/typebox";
 
-const baseIncentiveConfiguration = Type.Object({
+export const baseIncentiveConfiguration = Type.Object({
   enabled: Type.Boolean({ default: true }),
 });
 
 export type BaseConfiguration = Static<typeof baseIncentiveConfiguration>;
-
-export default baseIncentiveConfiguration;
